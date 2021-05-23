a0fe0bc409818b53e81484f6b83550720b132ab5
Euraka YLJ <galileoyulj@gmail.com>
2021-05-23 20:49:20 +0800

    update

---
+++ Part5/src/cn.edu.ncu.eureka.jdbc/JdbcTest.java
@@ -0,0 +1,18 @@
+package cn.edu.ncu.eureka.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JdbcTest {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tcph","root","root");
+        String sql = "update customer set address = '1' where custkey = 1";
+        Statement statement = connection.createStatement();
+        int count = statement.executeUpdate(sql);
+        System.out.println(count);
+        statement.close();
+        connection.close();
+    }
+}
